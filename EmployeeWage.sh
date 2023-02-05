fef6a892ebe115e6ddb95d5edb77cedf80a4c501
deepm24 <deep60640@gmail.com>
2023-02-05 18:31:34 +0530

    Added:UC1

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,1 +1,16 @@
 echo "welcome $name in wage Computation program on day_9_1 branch"
+ran=$((RANDOM%1))
+
+
+
+a=1
+
+
+if(($ran==$a))
+
+then
+      echo "prasent for full day"
+
+else
+      echo "absent"
+fi
