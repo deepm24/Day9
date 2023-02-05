3546f37e5c090c41f3175dbb42a035d002b099c5
deepm24 <deep60640@gmail.com>
2023-02-05 18:40:41 +0530

    added:UC3

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,16 +1,28 @@
 echo "welcome $name in wage Computation program on day_9_1 branch"
-ran=$((RANDOM%1))
+phwages=20
 
+fullday=8
+halfday=4
 
+ran=$((RANDOM%2))
 
 a=1
+b=2
 
 
 if(($ran==$a))
 then
       echo "prasent for full day"
-      dayWage=$((dhour * phwages))
-echo  "per day wage $dayWage"
+      dayWage=$((fullday * phwages))
+  echo "full day wage $dayWage"
+      
+      
+if(($ran==$b))
+then
+      echo "prasent for  half day"
+      dayWage=$((halfday * phwages))
+echo  "half day wage $dayWage"
+
 
 
 else
